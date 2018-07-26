444ab38d2e1d405d2eee27203914fbe3b29c7b2f
jarom <jarom.rial@gmail.com>
2018-07-26 16:34:19 -0600

    Added more relatives

--- mvc/src/com/jr/Constants.java
+++ mvc/src/com/jr/Constants.java
@@ -16,6 +16,22 @@ public class Constants {
         { "Jefferson Newton Jackson", "10 Nov 1903", "1 Nov 1988", "Great-Grandfather" },
         { "Farol Ballard Richardson", "27 Aug 1917", "10 Jun 2006", "Great-Grandmother" },
         { "Donald Edwin Higley", "17 Sep 1919", "16 Nov 1987", "Great-Grandfather" },
-        { "Thelma Christina Anderson", "7 Jul 1919", "3 Jan 2009", "Great-Grandmother" }
+        { "Thelma Christina Anderson", "7 Jul 1919", "3 Jan 2009", "Great-Grandmother" },
+        { "William David Rial", "28 Mar 1865", "23 Mar 1949", "2nd Great-Grandfather" },
+        { "Adalade Clementina Golding", "25 Nov 1874", "17 Feb 1945", "2nd Great-Grandmother" },
+        { "Oliver Pinkney Miles", "7 Jun 1878", "15 Mar 1939", "2nd Great-Grandfather" },
+        { "Melissa Mae Phillips", "21 Mat 1886", "24 Dec 1918", "2nd Great-Grandmother" },
+        { "William James Voss", "2 Sep 1839", "Oct 1891", "2nd Great-Grandfather" },
+        { "Mary Tenessee Williams", "10 Seo 1847", "25 Jan 1911", "2nd Great-Grandmother" },
+        { "Draughn William Marable", "28 Oct 1867", "13 Nov 1918", "2nd Great-Grandfather" },
+        { "Blanch Ethleen Sikes", "15 Dec 1877", "10 Mar 1922", "2nd Great-Grandmother" },
+        { "Charles Edward Jackson", "6 Sep 1876", "2 Feb 1924", "2nd Great-Grandfather" },
+        { "Minnie Rachael Ann Johnson", "6 Jul 1881", "26 Nov 1946", "2nd Great-Grandmother" },
+        { "Harold Darwin Richardson", "25 Mar 1889", "29 Nov 1962", "2nd Great-Grandfather" },
+        { "Abbie Day Ballard", "7 Dec 1891", "12 Apr 1987", "2nd Great-Grandmother" },
+        { "Charles Stacey Higley", "27 Jul 1876", "29 Dec 1949", "2nd Great-Grandfather" },
+        { "Martha Arizona Walters", "2 Feb 1885", "27 Sep 1952", "2nd Great-Grandmother" },
+        { "Lars Anderson", "23 Aug 1877", "17 Dec 1966", "2nd Great-Grandfather" },
+        { "Thekla Victoria Stromberg", "6 Jun 1889", "5 Mar 1966", "2nd Great-Grandmother" }
     };
 }
